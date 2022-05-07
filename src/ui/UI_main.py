eedae02472fd58e057c946ad0311f9c09a620abf
Lorin-lab <72334359+Lorin-lab@users.noreply.github.com>
2022-05-07 07:45:31 +0200

    fixe icon path

--- src/ui/UI_main.py
+++ src/ui/UI_main.py
@@ -2,6 +2,8 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
+from utils import *
+
 
 class UiMain(object):
     """This class contains all the widgets and configures them for the main window."""
@@ -34,31 +36,30 @@ class UiMain(object):
         # ****************************
         self.toolbar = QToolBar(main_window)
         main_window.addToolBar(Qt.LeftToolBarArea, self.toolbar)
-
         # config button
         self.client_config_tool_btn = QToolButton()
         self.client_config_tool_btn.setAutoRaise(True)
-        self.client_config_tool_btn.setIcon(QIcon("icons/outline_settings_black_24dp.png"))
+        self.client_config_tool_btn.setIcon(QIcon(resource_path("icons/outline_settings_black_24dp.png")))
         self.client_config_tool_btn.setToolTip("Communication settings")
         self.toolbar.addWidget(self.client_config_tool_btn)
 
         # connect button
         self.connect_tool_btn = QToolButton()
         self.connect_tool_btn.setAutoRaise(True)
-        self.connect_tool_btn.setIcon(QIcon("icons/outline_link_black_24dp.png"))
+        self.connect_tool_btn.setIcon(QIcon(resource_path("icons/outline_link_black_24dp.png")))
         self.connect_tool_btn.setToolTip("Connection")
         self.toolbar.addWidget(self.connect_tool_btn)
 
         # disconnect button
         self.disconnect_tool_btn = QToolButton()
         self.disconnect_tool_btn.setAutoRaise(True)
-        self.disconnect_tool_btn.setIcon(QIcon("icons/outline_link_off_black_24dp.png"))
+        self.disconnect_tool_btn.setIcon(QIcon(resource_path("icons/outline_link_off_black_24dp.png")))
         self.disconnect_tool_btn.setToolTip("Disconnection")
         self.toolbar.addWidget(self.disconnect_tool_btn)
 
         # Add com task button
         self.Add_section_tool_btn = QToolButton()
         self.Add_section_tool_btn.setAutoRaise(True)
-        self.Add_section_tool_btn.setIcon(QIcon("icons/outline_add_box_black_24dp.png"))
+        self.Add_section_tool_btn.setIcon(QIcon(resource_path("icons/outline_add_box_black_24dp.png")))
         self.Add_section_tool_btn.setToolTip("Add task")
         self.toolbar.addWidget(self.Add_section_tool_btn)
